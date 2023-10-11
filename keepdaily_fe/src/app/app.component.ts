480226b9130ca77e1f6ae44ac621b2ba84ccb3dc
InkyChew <inky11308@gmail.com>
2023-10-11 15:11:26 +0800

    Message component

--- keepdaily_fe/src/app/app.component.ts
+++ keepdaily_fe/src/app/app.component.ts
@@ -14,81 +14,25 @@ import { IAuthenticateUser } from './models/user';
 })
 export class AppComponent {
 
-  @ViewChild('msgitem') msgitem!: ElementRef;
-  user!: IAuthenticateUser;
+  
   isLogin: boolean = false;
   msgList: IMessage[] = [];
   newmsgs: IMessage[] = [];
   showMsg: boolean = false;
 
   constructor(private _userService: UserService,
-    private _router: Router,
-    private _msgService: MessageService,
-    private _hubService: HubService) {}
-
-  @HostListener('document:click', ['$event'])
-  onClick(event: Event) {
-    const target = event.target as HTMLElement;
-    const msgEl = this.msgitem?.nativeElement;
-
-    if (this.showMsg && !msgEl?.contains(target)) {
-      this.showMsg = false;
-      this.updateReadMsg();
-    }
-  }
+    private _router: Router) {}
 
   ngOnInit() {
     this._userService.user$.subscribe(user => {
       this.isLogin = user ? true : false;
-      if(user && this.isLogin) {
-        this.user = user;
+      if(this.isLogin) {
         this._router.navigateByUrl('/main');
-
-        this.getMsgList();
-
-        // Msg Listener
-        this._hubService.startConnection()
-        .pipe(
-          concatMap(() => this._hubService.setUserIdentifier(`${user.id}`)),
-          concatMap(() => this._hubService.onMessageReceived())
-        ).subscribe(msg => {
-          this.msgList.unshift(msg);
-          this.filterNewMsg();
-        });
-      } else this._hubService.stopConnection();
+      }
     });
-  }
-  
-  getMsgList() {
-    this._msgService.getMessages(this.user.id).subscribe(msgs => {
-      this.msgList = msgs;
-      this.filterNewMsg();
-    });
-  }
-
-  filterNewMsg() {
-    this.newmsgs = this.msgList.filter(_ => !_.isRead);
-  }
-
-  updateReadMsg() {
-    if(this.newmsgs.length > 0)
-      this._msgService.updateReadMessage(this.newmsgs).subscribe(() => this.getMsgList());
-  }
-
-  openMsgBox() {
-    if(this.showMsg) this.updateReadMsg();
-    this.showMsg = !this.showMsg;
-  }
-
-  deleteMsg(id: number) {
-    this._msgService.deleteMessage(id).subscribe(() => this.getMsgList());
   }
 
   logout() {
     this._userService.logout();
-  }
-
-  ngOnDestroy() {
-    this._hubService.stopConnection();
   }
 }
--- keepdaily_fe/src/app/messages/messages.component.ts
+++ keepdaily_fe/src/app/messages/messages.component.ts
@@ -1,6 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { IMessage } from '../models/message';
 import { environment } from 'src/environments/environment';
+import { MessageService } from '../services/message.service';
+import { HubService } from '../services/hub.service';
+import { UserService } from '../services/user.service';
+import { IAuthenticateUser } from '../models/user';
+import { concatMap } from 'rxjs';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -8,19 +13,77 @@ import { environment } from 'src/environments/environment';
 })
 export class MessagesComponent implements OnInit {
 
-  @Input() messages: IMessage[] = [];
-  @Input() show: boolean = false;
-  @Output() onDeleteMsg = new EventEmitter<number>();
+  user!: IAuthenticateUser;
+  msgList: IMessage[] = [];
+  newmsgs: IMessage[] = [];
+  show: boolean = false;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef,
+    private _userService: UserService,
+    private _msgService: MessageService,    
+    private _hubService: HubService) { }
 
-  ngOnInit(): void { }
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    const target = event.target as HTMLElement;
+    const msgEl = this.elementRef.nativeElement;
+
+    if (this.show && !msgEl?.contains(target)) {
+      this.show = false;
+      this.updateReadMsg();
+    }
+  }
+
+  ngOnInit(): void {
+    this._userService.user$.subscribe(user => {
+      if(user) {
+        this.user = user;
+
+        this.getMsgList();
+
+        // Msg Listener
+        this._hubService.startConnection()
+        .pipe(
+          concatMap(() => this._hubService.setUserIdentifier(`${user.id}`)),
+          concatMap(() => this._hubService.onMessageReceived())
+        ).subscribe(msg => {
+          this.msgList.unshift(msg);
+          this.filterNewMsg();
+        });
+      } else this._hubService.stopConnection();
+    });
+  }
 
   getImg(img: string) {
     return `${environment.api.url}/${img}`
   }
 
-  delMsg(id: number) {
-    this.onDeleteMsg.emit(id);
+  getMsgList() {
+    this._msgService.getMessages(this.user.id).subscribe(msgs => {
+      this.msgList = msgs;
+      this.filterNewMsg();
+    });
+  }
+
+  filterNewMsg() {
+    this.newmsgs = this.msgList.filter(_ => !_.isRead);
+  }
+
+  updateReadMsg() {
+    if(this.newmsgs.length > 0)
+      this._msgService.updateReadMessage(this.newmsgs).subscribe(() => this.getMsgList());
+  }
+
+  openMsgBox() {
+    if(this.show) this.updateReadMsg();
+    this.show = !this.show;
+  }
+
+  deleteMsg(id: number) {
+    this._msgService.deleteMessage(id).subscribe(() => this.getMsgList());
+  }
+
+  ngOnDestroy() {
+    this._hubService.stopConnection();
   }
 }
